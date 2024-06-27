fb42e5a071a8cbcb3061338834dbe33ba7d22c82
DGaoGao <2605951340@qq.com>
2024-06-27 14:19:44 +0800

    社保公积金界面(新增、删除和搜索功能完成)

--- saas_front/src/views/salary/BenefitsAndProvidentFund.vue
+++ saas_front/src/views/salary/BenefitsAndProvidentFund.vue
@@ -7,11 +7,16 @@
                 <!-- 搜索区域 -->
                 <div style="flex: 8;">
                     <el-form :inline="true">
-                        <el-form-item label="根据月份查询">
-                          <el-input placeholder="请输入查询内容" prefix-icon="el-icon-search"
+                        <el-form-item label="根据缴纳项目查询">
+                          <el-input placeholder="请输入缴纳项目名称" prefix-icon="el-icon-search"
                                     v-model="params.paymentProject"></el-input>
-                          <el-input placeholder="请输入查询内容" prefix-icon="el-icon-search"
-                                    v-model="params.state"></el-input>
+                        </el-form-item>
+                        <el-form-item label="根据状态查询">
+                          <el-select v-model="params.state" placeholder="请选择状态">
+                            <el-option value=""></el-option>
+                            <el-option label="启用" value="1"></el-option>
+                            <el-option label="禁用" value="0"></el-option>
+                          </el-select>
                         </el-form-item>
                         <el-form-item>
                             <el-button @click="search" type="primary" icon="el-icon-search">搜索</el-button>
@@ -27,9 +32,14 @@
             <el-card>
                 <span
                     style="font-size: 16px;font-weight: bold;font-family: 'Times New Roman', Times, serif;">社保公积金设置</span>
+              <div style="border: 2px; margin-top: 20px;">
+                <el-button type="primary" @click="addopen">新增</el-button>
+              </div>
             </el-card>
 
             <el-table :data="list" style="width: 100%">
+              <el-table-column type="selection" width="55">
+              </el-table-column>
                 <el-table-column prop="id" label="序号"> </el-table-column>
                 <el-table-column prop="paymentProject" label="缴纳项目"> </el-table-column>
                 <el-table-column prop="companyRatio" label="公司缴纳比例"> </el-table-column>
@@ -38,7 +48,7 @@
                   <template slot-scope="scope">
                     <!-- 使用条件判断显示具体信息 -->
                     <span v-if="scope.row.state === 1">启用</span>
-                    <span v-else-if="scope.row.state === 2">禁用</span>
+                    <span v-else-if="scope.row.state === 0">禁用</span>
                   </template>
                 </el-table-column>
                 <el-table-column prop="description" label="备注"> </el-table-column>
@@ -80,7 +90,7 @@
           <el-form-item label="状态信息">
             <el-select style="width: 100px; margin-left: 10px;" v-model="userFormData.state">
               <el-option label="启用" :value="1"></el-option>
-              <el-option label="禁用" :value="2"></el-option>
+              <el-option label="禁用" :value="0"></el-option>
             </el-select>
           </el-form-item>
 
@@ -101,8 +111,24 @@
       <el-dialog title="新增摘要内容" :visible.sync="addshow" width="600px">
         <el-form :model="userFormData" class="demo-form-inline" label-width="80px">
 
-          <el-form-item label="摘要">
-            <el-input type="textarea" v-model="userFormData.summaryContent" placeholder="摘要" :autosize="{ minRows: 4, maxRows: 6 }"></el-input>
+          <el-form-item label="缴纳项目">
+            <el-input v-model="userFormData.paymentProject" placeholder="缴纳项目名称"></el-input>
+          </el-form-item>
+
+          <el-form-item label="公司缴纳">
+            <el-input v-model="userFormData.companyRatio" placeholder="公司缴纳比例"></el-input>
+          </el-form-item>
+
+          <el-form-item label="个人缴纳">
+            <el-input v-model="userFormData.personalRatio" placeholder="个人缴纳比例"></el-input>
+          </el-form-item>
+
+          <el-form-item label="备注">
+            <el-input v-model="userFormData.description" placeholder="备注信息"></el-input>
+          </el-form-item>
+
+          <el-form-item label="租户id">
+            <el-input v-model="userFormData.tenantId" placeholder="租户id"></el-input>
           </el-form-item>
 
           <!-- 使用 Flex 布局排列按钮 -->
@@ -132,8 +158,8 @@ export default {
           total: 0,
           // 搜索条件
           params: {
-            paymentProject: null,
-            status: null
+            paymentProject: '',
+            state: null
           },
           // 新增/修改 内容
           userFormData:{
@@ -145,7 +171,8 @@ export default {
             state: '',
             tenantId: ''
           },
-          show:false
+          show:false,
+          addshow:false
         };
     },
     created() {
@@ -189,12 +216,35 @@ export default {
           this.userFormData.state = row.state;
           this.userFormData.tenantId = row.tenantId;
         },
+      addopen() {
+        this.addshow = true
+        this.userFormData.companyRatio =  '',
+        this.userFormData.description =  '',
+        this.userFormData.paymentProject =  '',
+        this.userFormData.personalRatio = '',
+        this.userFormData.id = '',
+        this.userFormData.state =  1,
+        this.userFormData.tenantId = ''
+      },
+      cancel() {
+        this.show = false
+        this.addshow = false
+      },
+      async out(row) {
+
+          const data = {
+            id: row.id,
+          }
+            const res = await axios({
+              url: "http://localhost:8081/accumulation/updateStatus",
+              method: "post",
+              params: data
+            })
+            this.getList();
+      },
       async submit() {
         const data = {
           ...this.userFormData,
-          if(){
-
-          }
         }
         const res = await axios({
           url: "http://localhost:8081/accumulation/update",
@@ -203,6 +253,18 @@ export default {
         })
         this.getList()
         this.show = false
+      },
+      async addsubmit() {
+        const data = {
+          ...this.userFormData
+        }
+        const res = await axios({
+          url: "http://localhost:8081/accumulation/add",
+          method: "post",
+          data: data
+        })
+        this.getList()
+        this.addshow = false
       }
 
     },
